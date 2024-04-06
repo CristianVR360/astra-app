47d914b458c5ca4d3e4814f32531ca89590cd240
CristianVR360 <cristiantrivinom@gmail.com>
2024-04-06 20:46:39 -0300

    2.2.3 bug eslint

--- src/components/common/FooterProyectosInmobiliarios/FooterProyectosInmobiliarios.jsx
+++ src/components/common/FooterProyectosInmobiliarios/FooterProyectosInmobiliarios.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faYoutube, faWhatsapp,  faWaze} from '@fortawesome/free-brands-svg-icons';
+import {  faInstagram, faYoutube, faWhatsapp,  faWaze} from '@fortawesome/free-brands-svg-icons';
 import './FooterProyectosInmobiliarios.css';
 
 const FooterProyectosInmobiliarios = ({ instagramUrl, ubicacionUrl, whatsappNumber, nombreProyecto, message, videoUrl, colorPrimario, colorSecundario }) => {
@@ -14,16 +14,22 @@ const FooterProyectosInmobiliarios = ({ instagramUrl, ubicacionUrl, whatsappNumb
 
   return (
     <footer className="footer__proyectos-inmobiliarios" style={footerStyle}>
+
+
+
+
       <div className="footer__proyectos-inmobiliarios-row">
-        <a href="#"><FontAwesomeIcon icon={faFacebook} className="footer__proyectos-inmobiliarios__icono" /></a>
-        <a href={instagramUrl} target="_blank"><FontAwesomeIcon icon={faInstagram} className="footer__proyectos-inmobiliarios__icono" /></a>
-        <a href={videoUrl} target="_blank"><FontAwesomeIcon icon={faYoutube} className="footer__proyectos-inmobiliarios__icono" /></a>
-        <a href={whatsappLink} target="_blank"><FontAwesomeIcon icon={faWhatsapp} className="footer__proyectos-inmobiliarios__icono" /></a>
-        <a href={ubicacionUrl} target="_blank"><FontAwesomeIcon icon={faWaze} className="footer__proyectos-inmobiliarios__icono" /></a>
+        
+        <a href={instagramUrl} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className="footer__proyectos-inmobiliarios__icono" /></a>
+        <a href={videoUrl} target="_blank" rel="noreferrer" ><FontAwesomeIcon icon={faYoutube} className="footer__proyectos-inmobiliarios__icono" /></a>
+        <a href={whatsappLink} target="_blank" rel="noreferrer" ><FontAwesomeIcon icon={faWhatsapp} className="footer__proyectos-inmobiliarios__icono" /></a>
+        <a href={ubicacionUrl} target="_blank" rel="noreferrer" ><FontAwesomeIcon icon={faWaze} className="footer__proyectos-inmobiliarios__icono" /></a>
 
       </div>
       <div className="footer__proyectos-inmobiliarios-row">
-        <a className='footer__proyectos-inmobiliarios__contacto'>{whatsappNumber}</a>
+      
+      <span className='footer__proyectos-inmobiliarios__contacto'>{whatsappNumber}</span>
+        
       </div>
       <div className="footer__proyectos-inmobiliarios-row">
         <p>
