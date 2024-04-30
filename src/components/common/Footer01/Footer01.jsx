fe58769915f331cd5d4b65359eaf7d6367eff188
CristianVR360 <cristiantrivinom@gmail.com>
2024-04-30 12:55:28 -0400

    detalles prcelas villarrica

--- src/components/common/Footer01/Footer01.jsx
+++ src/components/common/Footer01/Footer01.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { FaInstagram, FaFacebook, FaMapMarkerAlt } from "react-icons/fa";
 
-const Footer01 = ({ instagramLink, facebookLink, gpsLink, colorSecundario, telefono,footerAfterStyles }) => {
+const formatTelefono = (telefono) => {
+  if (telefono.length === 12) {
+    return `${telefono.slice(0, 4)} ${telefono.slice(4, 8)} ${telefono.slice(8)}`;
+  }
+  return telefono;
+};
+
+const Footer01 = ({ instagramLink, facebookLink, gpsLink, colorSecundario, telefono, footerAfterStyles }) => {
+  const formattedTelefono = formatTelefono(telefono);
+
   return (
-    <div className="footer01" >
+    <div className="footer01">
       <div className="footer01__social-links">
         <a href={instagramLink} className="footer01__social-link" style={{ color: colorSecundario }}>
           <FaInstagram className="footer01__social-icon" />
@@ -17,11 +26,10 @@ const Footer01 = ({ instagramLink, facebookLink, gpsLink, colorSecundario, telef
       </div>
       <p className="footer01__telefono" style={{ color: colorSecundario }}>
         <a href={`tel:${telefono}`} className="footer01__telefono-link">
-          {telefono}
+          {formattedTelefono}
         </a>
       </p>
-      <div className="footer01__after" style={footerAfterStyles}>
-        </div>
+      <div className="footer01__after" style={footerAfterStyles}></div>
     </div>
   );
 };
